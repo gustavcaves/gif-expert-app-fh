c9255bf6a060dbae5e5c748792560900c5bd52c4
gustavcaves <rungustavo@gmail.com>
2022-12-11 18:47:34 +0100

    creating a category list

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 
 import React from 'react'
 
 export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories2, setCategories2] = useState(['One Punch', 'Dragon Ball']);
+    const [categories3, setCategories3] = useState(['One Punch', 'Dragon Ball']);
+
+    console.log(categories)
+
   return (
     <>
+        {/* titulo */}
+        
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+        <ol>
+            { categories.map( category => {
+                return <li key={ category }>{ category}</li>
+            }) }
+            {/* <li>ABC</li> */}
+        </ol>
+            {/* Gif Item */}
+
     </>
   )
 }
