fd117dd6c7c6f2f10e0f17bdf18fb8f684354107
gustavcaves <rungustavo@gmail.com>
2022-12-22 01:56:05 +0100

    how to use a proxy to get data in private networks

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,31 @@
+import fetch from 'isomorphic-fetch';
+import HttpsProxyAgent from 'https-proxy-agent';
+
+// import proxyAgent from 'http-proxy-agent';
+// const options = {
+// 	agent: new proxyAgent('http://'),
+// };
+
+// const res = await fetch('http://api.tvmaze.com/search/shows?q=batman', 
+// {agent: new HttpsProxyAgent('<myproxy>')});
+
+// Esta fue la que me sirviop para obtener los datos utilizando el proxy
+// const resp = await fetch( url , {agent: new HttpsProxyAgent('<MyProxy>')});
+
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=a3KBu0lqCOSgx71tDfHAAwZ734xL4iR3&q=${category}&limit=10`
     const resp = await fetch( url );
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=a3KBu0lqCOSgx71tDfHAAwZ734xL4iR3&q=${category}&limit=10`
 
     const { data } = await resp.json();
 
+    
     const gifs = data.map(img => ({
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
     }));
+
+    console.log(gifs);
 
     return gifs;
 }
