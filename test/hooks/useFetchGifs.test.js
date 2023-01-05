7a4c6288d5651c51f2f9f0f43099a513cb9dc21d
gustavcaves <rungustavo@gmail.com>
2023-01-05 20:18:49 +0100

    useFetchGifs test

--- test/hooks/useFetchGifs.test.js
+++ test/hooks/useFetchGifs.test.js
@@ -4,29 +4,30 @@ import { useFetchGifs } from '../../src/hooks/useFetchGifs';
 
 describe('Pruebas en el kook useFetchGifs', () => {
     
-    test('debe de regresar el estado inicial', () => {
-
-        const { result } = renderHook( () => useFetchGifs('One Punch') );
-        const { images, isLoading } = result.current;
-        
-        expect( images.length ).toBe(0);
-        expect( isLoading ).toBeTruthy();
-
-    });
-
-    // test('debe de retornar un arreglo de imagenes y isLoading en false', async() => {
+    // test('debe de regresar el estado inicial', () => {
 
     //     const { result } = renderHook( () => useFetchGifs('One Punch') );
-        
-    //     await waitFor(
-    //         () => expect( result.current.images.length ).toBeGreaterThan(0)
-    //     );
-                
+    //     console.log(result)
     //     const { images, isLoading } = result.current;
         
-    //     expect( images.length ).toBeGreaterThan(0);
-    //     expect( isLoading ).toBeFalsy();
+    //     expect( images.length ).toBe(0);
+    //     expect( isLoading ).toBeTruthy();
 
     // });
+
+    test('debe de retornar un arreglo de imagenes y isLoading en false', async() => {
+
+        const { result } = renderHook( () => useFetchGifs('One Punch') );
+        
+        await waitFor(
+            () => expect( result.current.images.length ).toBeGreaterThan(0)
+        );
+                
+        const { images, isLoading } = result.current;
+        
+        expect( images.length ).toBeGreaterThan(0);
+        expect( isLoading ).toBeFalsy();
+
+    });
 
 });
